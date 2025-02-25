9894a2e55cad8ef2f2822e0572cb512507f41c0e
doxcom <aldoperez1319@gmail.com>
2025-02-24 23:44:50 -0600

    initial files for reactive practice

---
+++ src/main/java/com/example/demo_reactivo/DemoReactivoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_reactivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoReactivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoReactivoApplication.class, args);
+	}
+
+}

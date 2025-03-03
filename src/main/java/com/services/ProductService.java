ae630ab115ae9efafde0162998ee58b00391972b
doxcom <aldoperez1319@gmail.com>
2025-03-02 23:50:52 -0600

    adding mono reactive config to use kafka

--- src/main/java/com/services/ProductService.java
+++ src/main/java/com/services/ProductService.java
@@ -2,25 +2,27 @@ package com.services;
 
 import com.model.Product;
 import com.repository.ProductRepository;
+import org.slf4j.MDC;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+import reactor.util.context.Context;
 
 @Service
 public class ProductService {
 
-    //private final WebClient.Builder webClientBuilder = null;
     private final ProductRepository productRepository;
     private final WebClient webClient;
 
-    public ProductService(WebClient webClient, ProductRepository productRepository, WebClient webClient1) {
+    public ProductService(ProductRepository productRepository, WebClient.Builder webClientBuilder ) {
         this.productRepository = productRepository;
-        this.webClient = webClient1;
         this.webClient = webClientBuilder.baseUrl("http://apihere.com").build();
 
     }
 
-    public Mono<Product> getAndSaveProduct(Long id){
+    public Mono<Product> getAndSaveProductFromKafka(String name, Double precio, String traceId){
+      //add traceId to MDC, its a diagnostic context mapped
+        MDC.setContextMap(Context.of("traceId",traceId).stream().toMap());
         return webClient.get()
     }
 

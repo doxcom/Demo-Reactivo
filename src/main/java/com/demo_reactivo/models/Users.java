c0298d08daeef0ead517066c445b1b1699a1d8ee
doxcom <aldoperez1319@gmail.com>
2025-03-06 23:39:21 -0600

    creando tabla usuario y configurando conexion db para postgresql

---
+++ src/main/java/com/demo_reactivo/models/Users.java
@@ -0,0 +1,23 @@
+package com.demo_reactivo.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Data
+@Table
+@NoArgsConstructor
+@AllArgsConstructor
+public class Users {
+
+    @Id
+    private Long id;
+    private String name;
+    private String surname;
+    private String username;
+    private String email;
+    private String password;
+
+}

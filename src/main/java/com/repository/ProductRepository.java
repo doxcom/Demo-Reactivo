d0e2fe260bc24f6fbd10a19e1e94813a5d938680
doxcom <aldoperez1319@gmail.com>
2025-03-06 23:19:45 -0600

    refactorizando demo reactive

--- src/main/java/com/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.repository;
-
-import com.model.Product;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-public interface ProductRepository extends ReactiveCrudRepository<Product, Long> {
-}
